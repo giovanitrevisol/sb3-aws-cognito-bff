1a151d7fc5fcb8d394764a0805ab3617500c8d09
giovani <giovanitrevisol93@gmail.com>
2025-01-15 09:18:28 -0300

    demo congnito

--- src/main/java/br/dev/g2/cognnito_sb_demo/config/SecurityConfig.java
+++ src/main/java/br/dev/g2/cognnito_sb_demo/config/SecurityConfig.java
@@ -5,27 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Class to configure AWS Cognito as an OAuth 2.0 authorizer with Spring Security.
+ * In this configuration, we specify our OAuth Client.
+ * We also declare that all requests must come from an authenticated user.
+ * Finally, we configure our logout handler.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/auth/login").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutSuccessUrl("/"))
-                .oauth2ResourceServer(oauth2ResourceServer ->
-                        oauth2ResourceServer.jwt(Customizer.withDefaults())
-                )
-                .build();
+        CognitoLogoutHandler cognitoLogoutHandler = new CognitoLogoutHandler();
+
+        http.csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/").permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessHandler(cognitoLogoutHandler));
+        return http.build();
     }
-
-
 }
---
+++ src/main/java/br/dev/g2/cognnito_sb_demo/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package br.dev.g2.cognnito_sb_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * In this class, we tell SpringBoot to render and return our Thymeleaf html document.
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+}
